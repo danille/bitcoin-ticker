280b30b29eab4ac52249daeb4e385211b3f47a6f
Danylo Lebediev <danilleock@gmail.com>
2018-04-16 23:50:53 +0200

    Fetch price data, parse it and display to user.

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -1,24 +1,19 @@
-//
-//  ViewController.swift
-//  BitcoinTicker
-//
-//  Created by Angela Yu on 23/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
-
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
+
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
+    let currencySymbolArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
     var finalURL = ""
+    var currencySelected = ""
 
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
-    
 
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,64 +34,42 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
 
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(currencyArray[row])
-
+        currencySelected = currencySymbolArray[row]
         finalURL = baseURL + currencyArray[row]
-        print(finalURL)
+        getBitcoinPriceData(url: finalURL)
     }
 
 
-    
-    
-
-    
-    
-    
-//    
 //    //MARK: - Networking
-//    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
+    func getBitcoinPriceData(url: String) {
+
+        Alamofire.request(url, method: .get)
+                .responseJSON { response in
+                    if response.result.isSuccess {
+
+                        print("Sucess! Got the bitcoin price data")
+                        let priceJSON: JSON = JSON(response.result.value!)
+                        self.updateBitcoinPriceData(json: priceJSON)
+
+                    } else {
+                        print("Error: \(String(describing: response.result.error))")
+                        self.bitcoinPriceLabel.text = "Connection Issues"
+                    }
+                }
+
+    }
+
+
 //    //MARK: - JSON Parsing
-//    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
 
+    func updateBitcoinPriceData(json: JSON) {
 
+        if let priceResult = json["averages"]["day"].double {
+            bitcoinPriceLabel.text = "\(currencySelected)\(priceResult)"
+        } else {
+            bitcoinPriceLabel.text = "Price Unavailable"
+        }
+    }
 
 
 }
